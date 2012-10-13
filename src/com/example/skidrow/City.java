8775cab3a1e43b395ceaf4e43df66ec07675a669
skyyemen@gmail.com <skyyemen@gmail.com@fcf9d05a-7a26-a738-00b1-ac8c2198bbc2>
2012-10-13 03:36:30 +0000

    Creating the class City that handles giving the cities their properties
    git-svn-id: https://skid-row-team25.googlecode.com/svn/trunk@34 fcf9d05a-7a26-a738-00b1-ac8c2198bbc2

---
+++ src/com/example/skidrow/City.java
@@ -0,0 +1,19 @@
+package com.example.skidrow;
+
+/**
+ * It assigns the properties of cities 
+ * 
+ * @author Waleed
+ * @version 1.0
+ */
+public class City {
+	private String Name= " ";
+	
+	private int TechLevel = 0;
+	
+	public City(String CityName){
+		Name = CityName;
+	}
+	
+
+}
