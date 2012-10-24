7ffc7e03c64cb094eca7c02921d1ed2ce576cf5f
mh.underwood <mh.underwood@fcf9d05a-7a26-a738-00b1-ac8c2198bbc2>
2012-10-24 19:03:19 +0000

    Added new drug (Jenkem). Revisions to drugs to come.
    git-svn-id: https://skid-row-team25.googlecode.com/svn/trunk@55 fcf9d05a-7a26-a738-00b1-ac8c2198bbc2

---
+++ src/com/example/skidrow/Jenkem.java
@@ -0,0 +1,16 @@
+package com.example.skidrow;
+
+public class Jenkem extends Good{
+	
+	public Jenkem()
+	{
+		super(5);
+	}
+
+	@Override
+	int getBasePriceDuringEvent(String event) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
