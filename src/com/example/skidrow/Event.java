da91e77458c0364209a4ad2dadc17aa1b16d44db
pavia.andres@gmail.com <pavia.andres@gmail.com@fcf9d05a-7a26-a738-00b1-ac8c2198bbc2>
2012-10-25 02:02:51 +0000

    Added Event
    git-svn-id: https://skid-row-team25.googlecode.com/svn/trunk@57 fcf9d05a-7a26-a738-00b1-ac8c2198bbc2

---
+++ src/com/example/skidrow/Event.java
@@ -0,0 +1,15 @@
+package com.example.skidrow;
+/**
+ * Class that represents an event created by the simulator
+ * @author apavia3
+ *
+ */
+public class Event {
+	private String description;
+	private String name;
+	public Event(String name, String description){
+		this.name=name;
+		this.description=description;
+	}
+
+}
