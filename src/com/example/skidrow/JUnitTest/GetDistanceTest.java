069af5f8e21d703c00d7cf7e40be116a7121ad72
pavia.andres@gmail.com <pavia.andres@gmail.com@fcf9d05a-7a26-a738-00b1-ac8c2198bbc2>
2012-11-20 00:39:41 +0000

    Changed the name of the variables in GetDistanceTest (they were "too short")
    git-svn-id: https://skid-row-team25.googlecode.com/svn/trunk@93 fcf9d05a-7a26-a738-00b1-ac8c2198bbc2

--- src/com/example/skidrow/JUnitTest/GetDistanceTest.java
+++ src/com/example/skidrow/JUnitTest/GetDistanceTest.java
@@ -8,39 +8,52 @@ import com.example.skidrow.Game;
 import junit.framework.TestCase;
 
 public class GetDistanceTest extends TestCase {
-	City a=new City("City A",0,0);
-	City b=new City("City B",3,0);
-	City c=new City("City C",0,4);
-	City d=new City("City D",8,0);
-	City e=new City("City E",0,6);
-	City f=new City("City F",5,5);
-	City g=new City("City G",6,6);
+	City cityA=new City("City A",0,0);
+	City cityB=new City("City B",3,0);
+	City cityC=new City("City C",0,4);
+	City cityD=new City("City D",8,0);
+	City cityE=new City("City E",0,6);
+	City cityF=new City("City F",5,5);
+	City cityG=new City("City G",6,6);
 	Game tester = new Game();	
+	//@Test
+	  public void testDistanceToItself() {    
+		    tester.setCurrentCity(cityA);
+	    	assertEquals("Distance from city A to city A should be 0.", "0.000", tester.getDistance(cityA));
+	  }
 	  //@Test
 	  public void testHorizontalDistance() {    
-		    tester.setCurrentCity(a);
-	    	assertEquals("Distance from city A to city B should be 3.", "3.000", tester.getDistance(b));
+		    tester.setCurrentCity(cityA);
+	    	assertEquals("Distance from city A to city B should be 3.", "3.000", tester.getDistance(cityB));
 	  }
 	  //@Test
 	  public void testVerticallDistance() {    
-		    tester.setCurrentCity(a);
-	    	assertEquals("Distance from city A to city C should be 4.", "4.000", tester.getDistance(c));
+		    tester.setCurrentCity(cityA);
+	    	assertEquals("Distance from city A to city C should be 4.", "4.000", tester.getDistance(cityC));
 	  }
 	  public void testDistance1() {    
-		    tester.setCurrentCity(b);
-	    	assertEquals("Distance from city B to city C should be 5.", "5.000", tester.getDistance(c));
+		    tester.setCurrentCity(cityB);
+	    	assertEquals("Distance from city B to city C should be 5.", "5.000", tester.getDistance(cityC));
 	  }
 	  public void testDistance2() {    
-		    tester.setCurrentCity(d);
-	    	assertEquals("Distance from city D to city E should be 10.", "10.000", tester.getDistance(e));
+		    tester.setCurrentCity(cityD);
+	    	assertEquals("Distance from city D to city E should be 10.", "10.000", tester.getDistance(cityE));
 	  }
 	  public void testDistance3() {    
-		    tester.setCurrentCity(f);
-	    	assertEquals("Distance from city F to city G should be 1.414.", "1.414", tester.getDistance(g));
+		    tester.setCurrentCity(cityF);
+	    	assertEquals("Distance from city F to city G should be 1.414.", "1.414", tester.getDistance(cityG));
 	  }
 	  public void testDistance4() {    
-		    tester.setCurrentCity(a);
-	    	assertEquals("Distance from city F to city G should be 8.485.", "8.485", tester.getDistance(g));
+		    tester.setCurrentCity(cityA);
+	    	assertEquals("Distance from city a to city G should be 8.485.", "8.485", tester.getDistance(cityG));
+	  }
+	  public void testDistance5() {    
+		    tester.setCurrentCity(cityG);
+	    	assertEquals("Distance from city G to city F should be 1.414.", "1.414", tester.getDistance(cityF));
+	  }
+	  public void testDistance6() {    
+		    tester.setCurrentCity(cityG);
+	    	assertEquals("Distance from city G to city A should be 8.485.", "8.485", tester.getDistance(cityA));
 	  }
 
 }
